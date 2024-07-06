377ef484b9eb8a9ee4b4eb654a4d1fee27b9a09a
gerbie_goober <wymatthewwu@gmail.com>
2024-07-05 21:26:14 -0400

    bugfix: resolve dangling merge conflicts pt.2

--- backend/API/addSwipe.js
+++ backend/API/addSwipe.js
@@ -1,20 +1,14 @@
 const Swipes = require('../schema/swipes');
-<<<<<<< HEAD
 const Resume = require('../schema/resume');
-=======
->>>>>>> feature/LC-27-trending-resumes-page
 
 const addSwipe = async (req, res) => {
     try {
         const { user_id, resume_id, uploader_id, accept } = req.body;
-<<<<<<< HEAD
         const existingSwipe = await Swipes.findOne({ user_id, resume_id, accept });
 
         if (existingSwipe) {
             return res.status(400).json({ error: 'Duplicate swipe detected' });
         }
-=======
->>>>>>> feature/LC-27-trending-resumes-page
 
         // Create a new instance of Swipes
         const swipe = new Swipes({
@@ -27,14 +21,11 @@ const addSwipe = async (req, res) => {
         // Save the swipe to the database
         await swipe.save();
 
-<<<<<<< HEAD
         // update resume table to increment number of swipes only if user swiped right on user
         if (accept) {
             await Resume.findByIdAndUpdate(resume_id, { $inc: { num_swipes: 1 } });
         }
 
-=======
->>>>>>> feature/LC-27-trending-resumes-page
         res.status(200).json({ message: 'Swipe added successfully' });
     } catch (error) {
         console.error('Error adding swipe:', error);
